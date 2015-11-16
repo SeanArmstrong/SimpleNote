dfb5fae5fca4233164f21f57a92921242fa7c01a
Sean Armstrong <sean.armstrong@sage.com>
2015-11-16 08:29:40 +0000

    add material-ui

--- SimpleNoteReact/src/app.js
+++ SimpleNoteReact/src/app.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import tapEvent from "react-tap-event-plugin";
 
 import { Route, Router } from 'react-router';
 
 import Base from "./basecomponent";
+
+tapEvent();
 
 ReactDOM.render((
   <Router>
--- SimpleNoteReact/src/basecomponent.js
+++ SimpleNoteReact/src/basecomponent.js
@@ -1,11 +1,14 @@
 import React from "react";
 import MiniNode from "./components/mini-note";
 import SmallNav from "./components/small-nav";
+import LeftNav from 'material-ui/lib/left-nav';
 
 export default React.createClass({
   render() {
+    let menuItems = [ ];
     return (
-      <div className="greeting">
+      <div>
+        <LeftNav />
         <MiniNode subject="Hello" body="body" />
         <SmallNav />
       </div>
